c1056bbb6dbc9bf3528592e10e18fa28e536202c
Dheeraj <dheerajkp810@gmail.com>
2026-04-11 15:11:39 +0530

    updated cors allowed origin

--- backend/main.py
+++ backend/main.py
@@ -367,6 +367,7 @@ app.add_middleware(
         "http://localhost:5174",
         "http://127.0.0.1:5174",
         "http://0.0.0.0:5173",
+        "https://tradeshift-india.netlify.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
